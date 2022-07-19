4d79546db608d68a7eb53fb87309316ece05a2b9
Ndeta Innocent <you@example.com>
2022-07-20 01:14:12 +0300

    using slice furthermore

--- src/exercise.rs
+++ src/exercise.rs
@@ -9,7 +9,7 @@ pub fn celcius_to_fehrenheit(c: f64) -> f64 {
     ((c * 9.0) / 5.0) + 32.0
 }
 
-pub fn api() {
+pub fn api_temp() {
     println!("Please input a temperature in fehrenheit!");
     let mut fehrenheit = String::new();
     io::stdin()
@@ -24,3 +24,35 @@ pub fn api() {
 }
 
 //Generate the nth fibonacci number
+pub fn api_fib() {
+    println!("What fibonacci number do you like!");
+    let mut n = String::new();
+    io::stdin().read_line(&mut n).expect("Failed to read line");
+    let n: u32 = match n.trim().parse() {
+        Ok(num) => num,
+        Err(_) => panic!("Please input a number not a char"),
+    };
+    let nth = nth_fib(n);
+    println!("The number of {}   {} ", n, nth);
+}
+
+pub fn nth_fib(n: u32) -> u32 {
+    if n == 1 {
+        return 1;
+    } else if n == 0 {
+        return 0;
+    }
+    nth_fib(n - 1) + nth_fib(n - 2)
+}
+
+pub fn determine_temp_from_string(temp_string: &str) -> &str {
+    let temp_string_bytes = temp_string.as_bytes();
+    for (i, &item) in temp_string_bytes.iter().enumerate() {
+        if item == b'F' || item == b'f' {
+            return &temp_string[0..i];
+        } else if item == b'C' || item == b'c' {
+            return &temp_string[0..i];
+        }
+    }
+    &temp_string[..]
+}
