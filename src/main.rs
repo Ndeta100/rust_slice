192fa06b4350992c316676c7b0f02505c8020b6b
Ndeta Innocent <you@example.com>
2022-07-20 01:15:04 +0300

    adding exeercise and fixing slice

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod exercise;
 mod slice;
 fn main() {
     println!("Hello, world!");
@@ -6,4 +7,11 @@ fn main() {
     println!("{}", first_word_run);
     let (start, end) = slice::second_word(&s);
     println!("The second word starts at {} and ends at {}", start, end);
+    let word_is = slice::second_word_is(&s);
+    println!("The second word is {}", word_is);
+    let f1 = exercise::celcius_to_fehrenheit(46.4);
+    let f2 = exercise::fehrenheit_to_celcius(76.7);
+    //println!("{}     {}", f1, f2);
+    //exercise::api();
+    exercise::api_fib();
 }
